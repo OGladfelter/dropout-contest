60ea1dafabcc1979c063f94a5574080c36e79738
ogladfelter <ogladfelter@gmail.com>
2022-01-26 22:20:57 -0600

    automate calc of ranks, accuracy scores

--- 2024/js/script.js
+++ 2024/js/script.js
@@ -1,3 +1,5 @@
+numberOfCandidates = 14;
+
 function IsMobileCard() {
     var check =  false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
@@ -23,9 +25,6 @@ function openTab(evt, cityName, bgColor) {
 /// data prep functions /////////////////////
 // kendall tau rank distance function
 function calculateKendallTauDistance(listA, listB){
-  console.log(listA)
-  console.log(listB)
-  console.log(" ")
   var distance = 0
 
   // get all potential non-self, non-duplicative pairs between the two arrays
@@ -56,16 +55,32 @@ var dropoutOrder = ["Marianne Williamson", "Cory Booker", "John Delaney", "Andre
 
 function dataPrep() { 
   d3.csv("data/scoreboardDataApril8.csv", function(data) {
-      data.forEach((d) => {
+      data.forEach(d => {
           d.prediction = [d['1'],d["2"],d["3"],d["4"],d["5"],d["6"],d["7"],d["8"],d["9"],d["10"],d["11"],d["12"],d["13"],d["14"]];
           d.kendallDistance = calculateKendallTauDistance(dropoutOrder, d.prediction);
-          // calc accuracy % here
-          // calc rank here
+          d.kendallNormal = (d.kendallDistance / (numberOfCandidates * (numberOfCandidates - 1) / 2)); // normalized tau score = tau_distance / (n * n-1 / 2)
+          d.accuracy = 100 - (d.kendallNormal * 100); // accuracy percentage = 100 - normalized score (which is 0-1) * 100
+      });
+      data = data.slice().sort((a, b) => d3.ascending(a.kendallDistance, b.kendallDistance)); // sort data ascending by kendall distance
+      // compute rank for each player
+      for (i = 0; i < data.length; i++) {
+        if (i == 0) {
+          data[i].rank = 1;
+        }
+        else if (data[i].kendallDistance == data[i-1].kendallDistance) {
+          data[i].rank = data[i-1].rank;
+        }
+        else {
+          data[i].rank = i + 1;
+        }
+      };
+      // add each player to leaderboard
+      var distanceToColorScale = d3.scaleLinear().domain([0, data.length]).range(["#333399","#8181df"]); // row color
+      data.forEach(d => {
+        addRow(d.rank, d.name, d.kendallDistance, d.accuracy.toFixed(1), distanceToColorScale(d.rank), d);
       });
   }); 
 };
-
-dataPrep();
 
 //////////////////////////////////////////////////////////
 
@@ -100,7 +115,6 @@ function changeLeaderboardOptions() {
 
 // functions for leaderboard
 function addRow(rank, name, kendallDistance, accuracy, rowColor, d) {
-
     var table = document.getElementById("leaderboardTable");
     var row = table.insertRow(-1);
 
@@ -148,17 +162,6 @@ function addRow(rank, name, kendallDistance, accuracy, rowColor, d) {
         document.getElementById("winner").innerHTML = d["14"];
     });
 
-}
-
-function readData() { 
-    d3.csv("data/scoreboardDataApril8.csv", function(data) {
-
-        var distanceToColorScale = d3.scaleLinear().domain([0, data.length]).range(["#333399","#8181df"]);
-
-        data.forEach((d, i) => {
-            addRow(i, d.name, d.score, Number(d.percentage).toFixed(1), distanceToColorScale(i), d);
-        });
-    }); 
 }
 
 d3.select("#playerPredictions").on("mouseover", function() {
@@ -410,6 +413,8 @@ function drawScoresLineplot() {
 
   //Read the data
   d3.csv("data/scoresOverTime.csv", function(data) {
+
+    console.log(data);
 
     // get a dataset of only round 1, for name purposes
     filtered = data.filter(function(row) {
@@ -1140,8 +1145,9 @@ function drawHeatmap() {
 
 function onPageLoad() {
 
+    dataPrep();
+
     // leaderboard
-    readData();
 
     // scores lineplot
     drawScoresLineplot();
